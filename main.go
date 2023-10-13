2c443cac69f850f1b052bab208db54fb1af305a6
JN720 <112584623+JN720@users.noreply.github.com>
2023-10-13 13:21:00 -0700

    initial

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("environmental variable file not found")
+	}
+	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		panic("redis connection failed")
+	}
+	rdb := redis.NewClient(opt)
+	connStr := os.Getenv("POSTGRES_URL")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic("postgres connection failed")
+	}
+	app := gin.Default()
+	app.GET("/", func(c *gin.Context) { indexGet(c, db, rdb) })
+	app.Run("localhost:8000")
+}
+
+func indexGet(c *gin.Context, db *sql.DB, rdb *redis.Client) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "hello world"})
+}

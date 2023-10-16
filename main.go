5ed954bb2f032488d04281520b91f88b2d8074bb
JN720 <112584623+JN720@users.noreply.github.com>
2023-10-15 21:36:52 -0700

    implemented local user get

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -13,7 +14,6 @@ import (
 
 func main() {
 	err := godotenv.Load(".env")
-
 	if err != nil {
 		panic("environmental variable file not found")
 	}
@@ -29,9 +29,30 @@ func main() {
 	}
 	app := gin.Default()
 	app.GET("/", func(c *gin.Context) { indexGet(c, db, rdb) })
+	app.GET("/users/:id", func(c *gin.Context) { userGet(c, db, rdb) })
 	app.Run("localhost:8000")
 }
 
 func indexGet(c *gin.Context, db *sql.DB, rdb *redis.Client) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "hello world"})
+}
+
+func userGet(c *gin.Context, db *sql.DB, rdb *redis.Client) {
+	id, hasId := c.Params.Get("id")
+	if !hasId {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	var user struct {
+		Email   string `json:"email"`
+		Name    string `json:"name"`
+		Address string `json:"address"`
+	}
+	err := db.QueryRow("SELECT email, COALESCE(name, '') AS name, COALESCE(address, '') AS address FROM Users WHERE id = "+id+";").Scan(&user.Email, &user.Name, &user.Address)
+	if err != nil {
+		fmt.Println(err)
+		c.Status(http.StatusNotFound)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, user)
 }

65c86563142e2786453e70b8fb888c37757d55cf
JN720 <112584623+JN720@users.noreply.github.com>
2023-10-15 22:25:13 -0700

    added product creation

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -29,6 +30,7 @@ func main() {
 	app := gin.Default()
 	app.GET("/", func(c *gin.Context) { indexGet(c, db, rdb) })
 	app.GET("/users/:id", func(c *gin.Context) { userGet(c, db, rdb) })
+	app.POST("/products", func(c *gin.Context) { productPost(c, db, rdb) })
 	app.Run("localhost:8000")
 }
 
@@ -53,4 +55,32 @@ func userGet(c *gin.Context, db *sql.DB, rdb *redis.Client) {
 		return
 	}
 	c.IndentedJSON(http.StatusOK, user)
+}
+
+func productPost(c *gin.Context, db *sql.DB, rdb *redis.Client) {
+	var product struct {
+		Card        string `json:"card"`
+		Name        string `json:"name"`
+		Description string `json:"description"`
+		Quantity    string `json:"quantity"`
+		Price       string `json:"price"`
+	}
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	quantity, err1 := strconv.ParseInt(product.Quantity, 10, 16)
+	id, err2 := strconv.ParseInt(product.Card, 10, 32)
+	if err1 != nil || err2 != nil || quantity < 1 || id < 1 {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	_, err := db.Query("INSERT INTO Products(card_id, name, description, quantity, price, status, created) VALUES(" +
+		product.Card + ",'" + product.Name + "', '" + product.Description + "', " + product.Quantity + ", " + product.Price + ", 'A', NOW());")
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	c.Status(http.StatusCreated)
 }
